573b764831291929abfc7a21023f91f7378674ab
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-16 17:47:08 +0530

    authController

---
+++ controllers/authController.js
@@ -0,0 +1,11 @@
+const User = require('../models/User');
+const OTP = require('../models/OTP');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { sendOTPEmail } = require('../utils/email');
+
+const generateOTP = () => Math.floor(100000 + Math.random() * 900000).toString();
+
+const generateToken = (id, role) => {
+    return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
