45761b40dbd66254e06bdcb413d2efc1d4a1dee8
Balak Raj Chaudhari <balakraj23@gmail.com>
2026-06-17 16:13:42 +0530

    added eventController.js

---
+++ controllers/eventController.js
@@ -0,0 +1,14 @@
+const Event = require('../models/Event');
+
+exports.getEvents = async (req, res) => {
+    try {
+        const filters = {};
+        if (req.query.category) filters.category = req.query.category;
+        if (req.query.search) filters.title = { $regex: req.query.search, $options: 'i' };
+
+        const events = await Event.find(filters).populate('createdBy', 'name email');
+        res.json(events);
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
